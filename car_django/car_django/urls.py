f3835c1ae89bf622cfa7aeae3999d508ed1a7a92
macallejonieszaidinvergeles <macallejon281@ieszaidinvergeles.org>
2022-03-20 15:43:40 +0100

    añadido views

--- car_django/car_django/urls.py
+++ car_django/car_django/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from car_django.views import inicio
+from car_django.views import*
+# from . import views
+# from views import*
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', inicio),
+    path('resultado/', resultado),
 ]
