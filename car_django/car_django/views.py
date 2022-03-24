050c42004f6f1920b9805b9423f70af23b7b43a1
macallejonieszaidinvergeles <macallejon281@ieszaidinvergeles.org>
2022-03-24 22:14:01 +0100

    json con toda la info

--- car_django/car_django/views.py
+++ car_django/car_django/views.py
@@ -1,4 +1,4 @@
-from statistics import linear_regression
+# from statistics import linear_regression
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.template.loader import get_template
@@ -9,6 +9,11 @@ from joblib import load
 from django.core.files import File
 import os
 import numpy as np
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.safestring import mark_safe
+from django.template import Library
+
 
 # import pandas as pd
 
@@ -20,12 +25,22 @@ def inicio(request):
         os.path.dirname(os.path.realpath(__file__)) + "/model_linear_regression.pkl",
         "rb",
     )
-
     model_linear_regression = load(linear_regression)
+
+    marcas_id = open(
+        os.path.dirname(os.path.realpath(__file__)) + "/toda_info.json",
+        "rb",
+    )
+
+    with marcas_id as file:
+        data = json.load(file)
+        data_json = json.dumps(data)
 
     if request.POST:
         # fuelTypeId	km	makeId	modelId	transmissionTypeId	year	cubicCapacity	doors	hp
-        data_usuario = np.array([[2, 41080, 46, 322.0, 2.0, 2015, 1329.0, 5.0, 99.0]])
+        fuelTypeId = request.POST['fuelTypeId']
+
+        data_usuario = np.array([[fuelTypeId, 41080, 46, 322.0, 2.0, 2015, 1329.0, 5.0, 99.0]])
 
         predict = model_linear_regression.predict(data_usuario)
 
@@ -35,7 +50,13 @@ def inicio(request):
             {"predict": predict, "model": model_linear_regression},
         )
 
-    return render(request, "inicio.html")
+    print("data json",data)
+    return render(request, "inicio.html",{"todo_info":data_json})
+
+
+
+
+
 
 
 def resultado(request):
